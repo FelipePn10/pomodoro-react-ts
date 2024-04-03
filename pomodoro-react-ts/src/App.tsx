ef2d955206502d68013ec69fafd12fdf61dcc3eb
Felipe Panosso <149212013+FelipePn10@users.noreply.github.com>
2024-04-02 22:07:20 -0300

    v1

---
+++ pomodoro-react-ts/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./Button/Button";
+
+export function App() {
+
+  return (
+    <div>
+      <Button />
+    </div>
+   
+  )
+}
+
+
---
+++ pomodoro-react-ts/src/Button/Button.tsx
@@ -0,0 +1,9 @@
+import { ButtonContainer } from "./Button.styles"
+
+export function Button() {
+    return (
+        <div>
+            <ButtonContainer></ButtonContainer>
+        </div>
+    )
+}
